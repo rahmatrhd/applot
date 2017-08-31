5c3b154f756bedee5a9dc2dca4a5e8054ed7b75d
hacktiv-8 <hacktiv-8@Hacktiv8-10.local>
2017-08-31 10:27:21 +0700

    cors added

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://applot:applot@applot-shard-00-00-00cap.mongodb.net:27017,applot-shard-00-01-00cap.mongodb.net:27017,applot-shard-00-02-00cap.mongodb.net:27017/test?ssl=true&replicaSet=applot-shard-0&authSource=admin')
 
 const app = express()
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
-
 
 //routing
 const index = require('./routes/index')
@@ -15,7 +16,6 @@ const storage = require('./routes/storage')
 
 app.use('/', index)
 app.use('/storage', storage)
-
 
 //listen
 app.listen(process.env.PORT || 3000, () => {
